2879ffec6a517e2dc6003183076c3cbe4509be91
vishnukumarraut <147373249+vishnukumarraut@users.noreply.github.com>
2024-01-03 18:00:56 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int b=64;     for(int i=1;i<=a;i++){         for(int j=1;j<=a;j++){             if(i+j==a+1 || i==j){                 char ch=b+i;                 printf("%c",ch);             }             else {                 printf(" ");             }         }         printf(" ");     } }

---
+++ Pattern_8.5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int b=64;
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<=a;j++){
+            if(i+j==a+1 || i==j){
+                char ch=b+i;
+                printf("%c",ch);
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("
+");
+    }
+}    
