5a66eca08ddce82e241bf0a8ce6427384d484b9e
vishnukumarraut <147373249+vishnukumarraut@users.noreply.github.com>
2024-02-14 18:20:34 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     for(int i=a;i>=1;i--){         for(int j=1;j<=a-i;j++){             printf(" ");         }         for(int k=1;k<=i;k++){             printf("%d ",i);         }     printf(" ");     } }

---
+++ Pattern_7.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    for(int i=a;i>=1;i--){
+        for(int j=1;j<=a-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++){
+            printf("%d ",i);
+        }
+    printf("
+");
+    }
+}    
+    
